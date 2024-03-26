ea01560b3382c20c395ec30a10bf95c9504faf13
Sachin <guitarplayersachin@gmail.com>
2024-03-27 03:51:20 +0530

    creating multipage webapp with Streamlit

--- chains.py
+++ chains.py
@@ -99,7 +99,7 @@ def call_api(AUDIO_URL):
         # STEP 2: Call the transcribe_url method on the prerecorded class
         options = PrerecordedOptions(
             model="nova-2",
-            smart_format=True,
+            smart_format=False,
             language= "hi"
         )
         url_response = deepgram.listen.prerecorded.v("1").transcribe_url(AUDIO_URL, options)
@@ -199,3 +199,123 @@ def chunkstring(string, length):
 # eng = translate_hindi_to_english(string2)
 # print(eng)
 # identify_contact(eng)
+
+
+####with phrases similar to training words
+
+
+def identify_greeting(text):
+    prompt = f"""
+The text is delimited with triple backticks. \
+The text is a conversation between two professionals \
+Identify these queries from the text : \
+    - In the text is anyone greeting the other person at the start properly , return value as true if yes and false if not. \
+       \
+    - Probability between 0 and 1 to previous query. \
+Format your response as JSON Object with \
+"contact_details" and "probability" as the keys.
+if contact details reponse is N/A replace it with false
+Review text: '''{text}'''
+"""
+    response = get_completion1(prompt)
+    print(response)
+    r1 = json.loads(response)
+    return r1
+
+text5 = "it is with pleasure i greet you, how are you? welcome to our app"
+
+text6 = "you dont matter anymore"
+
+
+def identify_silence(words,length_audio):
+    start = 0
+    end = length_audio
+    silence_ranges = []
+    print(float(words[0].start))
+    silence_ranges.append([start,float(words[0].start)])
+    for i in range(1,len(words)):
+        if(float(words[i-1].end) != float(words[i].start)):
+            silence_ranges.append([float(words[i-1].end),float(words[i].start)])
+    if(float(words[len(words)-1].end)!= end):
+        silence_ranges.append([float(words[len(words)-1].end),end])
+    print(silence_ranges)
+    return silence_ranges
+
+identify_greeting(text5)
+identify_greeting(text6)
+
+
+
+
+
+##############################
+
+
+from pydub import AudioSegment
+import requests
+import wave,wget
+
+def download_audio(url):
+    full_sounds = AudioSegment.empty()
+    #audio = AudioSegment.from_file("")
+    filename = wget.download(url)
+    sound = AudioSegment.from_file(filename, format='m4a')
+    file_handle = sound.export("1.wav", format='wav')
+    #open("file.wav", 'wb').write(resp.content)
+
+    #f = wave.open(audio_file, "r")
+
+
+
+def trim_audio(intervals, input_file_path, output_file_path):
+    # load the audio file
+    audio = AudioSegment.from_file(input_file_path)
+    combined_sounds = AudioSegment.empty()
+    # iterate over the list of time intervals
+    for i, (start_time, end_time) in enumerate(intervals):
+        # extract the segment of the audio
+        segment = audio[start_time*1000:end_time*1000]
+
+        # construct the output file path
+        #output_file_path_i = f"{output_file_path}_{i}.wav"
+        combined_sounds += segment
+        # export the segment to a file
+    combined_sounds.export(output_file_path, format='wav')
+
+# test it out
+print("Trimming audio...")
+#trim_audio([[0, 1], [1, 2]], "test_input.wav", "test_output")
+
+
+import numpy as np
+from scipy.io.wavfile import read
+
+def calculate_average_db(wav_file):
+  """Calculates the average dB level over a WAV file.
+
+  Args:
+    wav_file: The path to the WAV file.
+
+  Returns:
+    The average dB level.
+  """
+
+  # Read the WAV file.
+  sample_rate, wav_data = read(wav_file)
+
+  # Convert the WAV data to a float32 array.
+  wav_data = np.abs(wav_data.astype(np.float32)) / 32768
+
+  # Split the WAV data into chunks.
+  chunks = np.array_split(wav_data, 100)
+
+  # Calculate the average dB level for each chunk.
+  db_levels = [20 * np.log10(np.mean(chunk ** 2)) for chunk in chunks]
+
+  # Calculate the average dB level over the entire file.
+  average_db_level = np.mean(db_levels)
+
+  return average_db_level
+
+# # Example usage:
+
---
+++ pages/2_Chat.py
@@ -0,0 +1,255 @@
+#####
+import os
+import threading
+import logging, verboselogs
+
+
+from chains import *
+import streamlit as st
+
+from io import StringIO
+from uuid import uuid4 
+
+
+def second_thread():
+    progress_bar2 = st.progress(98)
+
+
+
+def run_on_csv(uploaded_file):
+    AUDIO_URL = {
+    "url": "https://s3-ap-southeast-1.amazonaws.com/exotelrecordings/aviga1/bae15c97eb96a712a492bae55fc6181e.mp3"
+
+}
+    sensitivity = 0.1
+    specific = 0.2
+    silence_threshold = 3  #### seconds of silence allowed
+    audio_csv_file = str(uploaded_file)
+    outfile_audio = "f3_mar3_111.csv"
+    outfile_errors = "f4_mar3_111.csv"
+    progress_bar = st.progress(0)
+    frame_text = st.empty()
+
+    #call_api(AUDIO_URL) #"id","astrologer_id","user_id","type","created_at"
+    id1 = read_variable_from_csv(audio_csv_file,"id")
+    astrologer_id = read_variable_from_csv(audio_csv_file,"astrologer_id")
+    user_id = read_variable_from_csv(audio_csv_file,"user_id")
+    type1 = read_variable_from_csv(audio_csv_file,"type")
+    created_at = read_variable_from_csv(audio_csv_file,"created_at")
+    url = read_variable_from_csv(audio_csv_file,"file_url")
+    order_item_id = read_variable_from_csv(audio_csv_file,"order_item_id")
+    star_rating = read_variable_from_csv(audio_csv_file,"Rating")
+    #Date = read_variable_from_csv(audio_csv_file,"Date")
+    l = []
+    background_noise = False
+    # #for i in range(0,len(url)):
+    total_urls = 2
+    for i in range(1,total_urls):
+        progress_bar.progress(i)
+        percentage = (i+1/total_urls)
+        frame_text.text("Percentage Completed %i" % percentage + "%")
+        url_base = url[i]
+        id1x=id1[i]
+        astrologer_idx = astrologer_id[i]
+        user_idx = user_id[i]
+        type1x = type1[i]
+        created_atx = created_at[i]
+        order_item_idx = order_item_id[i]
+        star_ratingx = star_rating[i]
+        print(url_base)
+        AUDIO_URL = {"url":url_base}
+        response_object = call_api(AUDIO_URL)
+
+        #print(response_object)
+        number = i
+        if(response_object):
+            print("line number is ",number)
+            sexual,hate,harrasment,self_harm,sexual_minor,threatening,violence,violence_graphic,self_harm_intent,self_harm_instructions,silence,if_any = (False,)*12
+            #write each response to new csv
+            transcript = response_object.results.channels[0].alternatives[0].transcript
+            confidence = response_object.results.channels[0].alternatives[0].confidence
+            print(transcript,confidence)
+
+            ##### silence range and thresold 
+            words = response_object.results.channels[0].alternatives[0].words
+            length_audio = response_object.metadata.duration
+            silence_ranges = identify_silence(words,length_audio)
+            download_audio(url_base)
+            trim_audio(silence_ranges,"1.wav","2.wav")
+            #calculate_average_db("test_output.wav")
+            finaldb = calculate_average_db('2.wav')
+            print("final",finaldb)
+            initialdb = calculate_average_db("1.wav")
+            print("initial",initialdb)
+            divide = initialdb/finaldb
+            if(divide>.80):
+                background_noise = True
+            for silence_range in silence_ranges:
+                if((float(silence_range[1])-float(silence_range[0]))>silence_threshold):
+                    silence = True
+                    if_any = True
+            list_transcript = list(chunkstring(transcript, 2000))
+            ####checking for greetings at start of transcript only
+            greeting_response = identify_greeting(list_transcript[0])
+
+            print(greeting_response)
+
+
+            #print(list_transcript)
+            for transcript in list_transcript:
+                eng_from_hindi = translate_hindi_to_english(transcript)
+                print(transcript,eng_from_hindi)
+                response = identify_classes(eng_from_hindi)
+                #intermediate area; a topic that is not clearly one thing or the other.
+
+                print(response["results"][0]["category_scores"])
+                # for scores in response["results"]:
+                #     print(scores)
+                #     print(type(scores))
+                sexual_score = (response["results"][0]["category_scores"]["sexual"])
+                if(sexual_score>sensitivity and sexual_score<specific):
+                    sexual = "intermediate"
+                    if_any = "intermediate"
+                elif(sexual_score>specific): 
+                    sexual= True
+                    if_any = True
+                hate_score = (response["results"][0]["category_scores"]["hate"])
+                if(hate_score>sensitivity and hate_score<specific):
+                    hate = "intermediate"
+                    if_any = "intermediate" 
+                elif(hate_score>specific): 
+                    hate= True
+                    if_any = True
+                harrasment_score = (response["results"][0]["category_scores"]["harassment"])
+                if(harrasment_score>sensitivity and harrasment_score<specific):
+                    harrasment = "intermediate"
+                    if_any = "intermediate" 
+                elif(harrasment_score>specific): 
+                    harrasment= True
+                    if_any = True    
+                self_harm_score = (response["results"][0]["category_scores"]["self-harm"])
+                if(self_harm_score>sensitivity and self_harm_score<specific):
+                    self_harm = "intermediate"
+                    if_any = "intermediate"
+                elif(harrasment_score>specific): 
+                    self_harm= True
+                    if_any = True
+                sexual_minor_score = (response["results"][0]["category_scores"]["sexual/minors"])
+                if(sexual_minor_score>sensitivity and self_harm_score<specific):
+                    sexual_minor= "intermediate"
+                    if_any = "intermediate"
+                elif(sexual_minor_score>specific): 
+                    sexual_minor= True
+                    if_any = True
+                threatening_score = (response["results"][0]["category_scores"]["hate/threatening"])
+                if(threatening_score>sensitivity and threatening_score<specific):
+                    threatening = "intermediate"
+                    if_any = "intermediate"
+                elif(threatening_score>specific): 
+                    threatening= True
+                    if_any = True
+                violence_graphic_score = (response["results"][0]["category_scores"]["violence/graphic"])
+                if(violence_graphic_score>sensitivity and violence_graphic_score<specific):
+                    violence_graphic = "intermediate" 
+                    if_any = "intermediate"
+                elif(violence_graphic_score>specific): 
+                    violence_graphic= True
+                    if_any = True
+                self_harm_intent_score = (response["results"][0]["category_scores"]["self-harm/intent"])
+                if(self_harm_intent_score>sensitivity and self_harm_intent_score<specific):
+                    self_harm_intent= "intermediate" 
+                    if_any = "intermediate"
+                elif(self_harm_intent_score>specific): 
+                    self_harm_intent= True
+                    if_any = True
+                self_harm_instructions_score = (response["results"][0]["category_scores"]["self-harm/instructions"])
+                if(self_harm_instructions_score>sensitivity and self_harm_instructions_score>specific):
+                    self_harm_instructions = "intermediate"
+                    if_any = "intermediate" 
+                elif(self_harm_instructions_score>specific): 
+                    self_harm_instructions= True
+                    if_any = True
+                violence_score = (response["results"][0]["category_scores"]["violence"])
+                if(violence_score>sensitivity and violence_score<specific):
+                    violence = "intermediate"
+                    if_any = "intermediate"
+                elif(violence_score>specific): 
+                    violence= True
+                    if_any = True
+                contact_response = identify_contact(eng_from_hindi)
+                contact_details = (contact_response["contact_details"])
+                probability = (contact_response["probability"])
+                if(probability>0.4 and probability<0.6):
+                    contact_details = "intermediate"
+                    if_any = "intermediate"
+                df = pd.DataFrame([[astrologer_idx,id1x,type1x,user_idx,created_atx,order_item_idx,star_ratingx,number,url_base,transcript,confidence,eng_from_hindi,sexual,sexual_score,hate,hate_score,harrasment,harrasment_score,self_harm,self_harm_score,sexual_minor,sexual_minor_score,threatening,threatening_score,violence_graphic,violence_graphic_score,self_harm_intent,self_harm_intent_score,self_harm_instructions,self_harm_instructions_score,violence,violence_score,contact_details,probability,silence,if_any]],
+                    columns=['astrologer_id','id1','type1','user_id','created_at','order_item_id','star_rating','number','url_base','transcript','confidence','eng_from_hindi','sexual','sexual_score','hate','hate_score','harrasment','harrasment_score','self_harm','self_harm_score','sexual_minor','sexual_minor_score','threatening','threatening_score','violence_graphic','violence_graphic_score','self_harm_intent','self_harm_intent_score','self_harm_instructions','self_harm_instructions_score','violence','violence_score','contact_details','probability','silence','if_any'])
+                print(df)
+                
+                hdr = False  if os.path.isfile(outfile_audio) else True
+                df.to_csv(outfile_audio, mode='a', header=hdr)
+                #df.to_csv('file_name.csv',mode='a+')
+                print(transcript)
+                sexual,hate,harrasment,self_harm,sexual_minor,threatening,violence,violence_graphic,self_harm_intent,self_harm_instructions,silence,if_any = (False,)*12
+        else:
+            l = 36 *["None"]
+            transcript = "None"
+            confidence = "None"
+            words = "None"
+            hdr = False  if os.path.isfile(outfile_errors) else True
+            df = pd.DataFrame([l],
+                columns=['astrologer_id','id1','type1','user_id','created_at','order_item_id','star_rating','number','url_base','transcript','confidence','eng_from_hindi','sexual','sexual_score','hate','hate_score','harrasment','harrasment_score','self_harm','self_harm_score','sexual_minor','sexual_minor_score','threatening','threatening_score','violence_graphic','violence_graphic_score','self_harm_intent','self_harm_intent_score','self_harm_instructions','self_harm_instructions_score','violence','violence_score','contact_details','probability','silence','if_any'])
+            print(df)
+            df.to_csv(outfile_errors,mode='a+')
+    with open("f3_mar3_111.csv", "rb") as template_file:
+        template_byte = template_file.read()
+        frame_text.text("Download Now")
+        progress_bar.progress(100)
+    st.download_button(label="Download Final Result",
+                        data=template_byte,
+                        file_name="f3_mar3_111.csv",
+                        mime='text/csv')
+    st.dataframe(df)
+
+
+    # parsed = json.loads(call_api(read_url_from_csv("recordings.csv")))
+    # df1=pd.DataFrame(parsed[""])
+    # df1.to_csv('csvfile.csv', encoding='utf-8', index=False)
+
+
+
+#run_on_csv()
+
+
+
+# Page title
+st.set_page_config(page_title='🦜🔗 Vama AI Chat Data CSV')
+st.title('🦜🔗 Vama AI Classify Chat Data')
+
+# File upload
+uploaded_file = st.file_uploader('Upload the Chat Text Data csv', type='csv')
+#= st.download_button('Download CSV for audio', text_contents, 'f3_mar3_111.csv', 'text/csv',disabled=True)
+
+
+download_instructions = st.empty()
+download_instructions.text("🦜🔗 Download button enables when 100% done")
+#with open('f3_mar3_111.csv') as f:
+#st.title('🦜🔗 Download button enables when 100% done')
+#download_final = st.download_button('Download Final CSV')  # Defaults to 'text/plain'
+
+#uploaded_file2 = st.file_uploader('Upload the text csv', type='csv')
+
+#data = read_csv(temp_filepath)
+
+if uploaded_file is not None:
+    content = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+    temp_filepath = f"/tmp/{uuid4()}"
+    with open(temp_filepath, "w") as f:
+        f.write(content)
+    #t1 = threading.Thread(target=run_on_csv, args=(temp_filepath))
+    #t1.start()
+    run_on_csv(temp_filepath)
+
+
+#if uploaded_file2 is not None:
+#    second_thread()
